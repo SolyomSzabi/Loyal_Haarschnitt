3600b5da342151d93de5be2d282143915d4b076a
SolyomSzabi <szabolcs.solyom@commitsmart.com>
2026-01-27 20:03:50 +0200

    fixed @ to . in indexjs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Toaster } from "@/components/ui/sonner";
+import { Toaster } from "./components/ui/sonner";
 import "./App.css";
 import "./i18n";
 
