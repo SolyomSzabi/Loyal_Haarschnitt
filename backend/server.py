0da7d0b31435acb448d02d9756240b1436f10d61
SolyomSzabi <szabolcs.solyom@commitsmart.com>
2026-06-28 02:40:37 +0300

    backend save

--- backend/server.py
+++ backend/server.py
@@ -823,7 +823,7 @@ Hier sind die Details Ihres Termins:
   • Uhrzeit: {appointment_obj.appointment_time.strftime("%H:%M")}
 
 Falls Sie Ihren Termin ändern oder stornieren möchten, kontaktieren Sie uns bitte:
-Telefon: +49 1573 5342854
+Telefon: +49 15569 167244
 
 Wir freuen uns auf Ihren Besuch bei Loyal Haarschnitt!
 
@@ -846,7 +846,7 @@ Here are the details of your appointment:
   • Time: {appointment_obj.appointment_time.strftime("%H:%M")}
 
 If you need to modify or cancel your appointment, please contact us:
-Phone: +49 1573 5342854
+Phone: +49 15569 167244
 
 We look forward to welcoming you at Loyal Haarschnitt!
 
